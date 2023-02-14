e387c21c555bf0e2ded7459434cdce06e7b649b2
GUICHARD Benoit <100837994+BenoitGUICHARD@users.noreply.github.com>
2023-02-14 11:56:17 +0100

    Add files via upload

---
+++ Matrix/Object.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Object.h"
+#include <string>
+
+class Object
+{
+#pragma region constructor/destructor
+public:
+	Object() = default;
+	Object(const Object& _copy) = default;
+	 
+	virtual ~Object() = default;
+#pragma endregion constructor/destructor
+#pragma region methods
+public:
+	virtual std::string ToString() const;
+#pragma endregion methods
+};
+
---
+++ Matrix/Utils.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+class Object;
+
+#pragma warning (disable 4996)
+
+#define color_white 15
+#define color_red 4
+
+#define SPACE 32
+#define ESCAPE 27
+
+class Utils
+{
+#pragma region Log
+public:
+	
+	static void LogWithEffect(const std::string& _msg, const int _time = 20); 
+	static inline HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	static inline COORD cursorPosition = {};
+	static void Log(const std::string& _msg);
+	static void Log(const Object& _obj);
+	static void Log(const Object* _obj);
+	static void LogError(const std::string& _msg);
+	static void LogError(const Object& _obj);
+	static void LogError(const Object* _obj);
+	static void LogTitle(const std::string& _title);
+	static void LoadingBar(const std::string& _msg);
+#pragma endregion Log
+#pragma region Utils
+public:
+	static int Random(const int _min, const int _max);
+	static void SetWindowSize(const int _width, const int _height);
+	static void Sleep(const int _milliSeconds);
+	static int CinNoBlock();
+	static void SetCursor(const bool _visible, const int _size); 
+	static void SetCursorPosition(const int _x, const int _y);
+	static std::string Underline(const std::string& _str);
+	static void ClearConsole();
+	static void Pause();
+	static std::string Separator(const int _count = 10, const char _c = '=');
+	template<typename T>
+	static T UserChoice(const std::string& _msg = "");
+	template<typename T>
+	static T UserChoice(const std::string& _msg, const T& _defaultValue, const T& _a, const T& _b);
+#pragma endregion Utils
+
+};
+
+template<typename T>
+inline T Utils::UserChoice(const std::string& _msg)
+{
+	if (_msg != "") Log(_msg);
+	T _result = T();
+	std::cin >> _result;
+	return _result;
+}
+
+template<typename T>
+inline T Utils::UserChoice(const std::string& _msg, const T& _defaultValue, const T& _a, const T& _b)
+{
+	if (_msg != "") Log(_msg);
+	T _result = _defaultValue;
+	while (_result != _a && _result != _b)
+		std::cin >> _result;
+	return _result;
+}
